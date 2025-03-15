831e382c19f26833c60ae6257b623947aa11ea57
Shreejith Kumar Das S <135417968+ArtsyDEV-C@users.noreply.github.com>
2025-03-15 09:40:02 +0530

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -1,32 +1,29 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const User = require('../models/User');
-const bcrypt = require('bcryptjs');
 
-passport.use(
-  new LocalStrategy(async (username, password, done) => {
+passport.use(new LocalStrategy(async (username, password, done) => {
     try {
-      const user = await User.findOne({ username });
-      if (!user) return done(null, false, { message: 'User not found' });
+        const user = await User.findOne({ username });
+        if (!user) return done(null, false, { message: '❌ User not found' });
 
-      const isMatch = await bcrypt.compare(password, user.password);
-      if (!isMatch) return done(null, false, { message: 'Incorrect password' });
+        const isMatch = await user.comparePassword(password);
+        if (!isMatch) return done(null, false, { message: '❌ Incorrect password' });
 
-      return done(null, user);
+        return done(null, user);
     } catch (error) {
-      return done(error);
+        return done(error);
     }
-  })
-);
+}));
 
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
-
-passport.deserializeUser((id, done) => {
-  User.findById(id, (err, user) => {
-    done(err, user);
-  });
+passport.serializeUser((user, done) => done(null, user.id));
+passport.deserializeUser(async (id, done) => {
+    try {
+        const user = await User.findById(id);
+        done(null, user);
+    } catch (error) {
+        done(error);
+    }
 });
 
 module.exports = passport;
